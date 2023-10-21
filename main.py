c7694612764826b941e187e7c7480806d2b7b46f
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-10-21 12:29:31 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Hotel", page_icon="🛏️")
+
+st.title('Novus Hotel 🛏️ Automated Cycle')
+
+st.header("Mandos (5) ⚙️ & Atentos (2)🤖")
+st.subheader("Beds, Talent, Expenditures, Sales & Optimal Ubications 🎯")
+st.subheader("New Sales & Confirmn, Welcoming & Support Clients")
