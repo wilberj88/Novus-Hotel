3290e4081095a1532958cfff7e4ce843c1c39306
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-10-23 00:26:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,72 @@ st.write('---')
 
 st.title('Configur your Novus Hotel 🛏️ right now ⏲️')
 
+st.subheader('MANDOS')
+col1, col2 = st.columns(2)
 
+with col1:
+    suits = st.selectbox(
+        "How many suits do you have?",
+        ("1", "2", "3", "4", "5"),
+    )
+    rooms = st.radio(
+        "How many rooms do you have?",
+        options=['10', '15','20', '25', '30', '35', '40', '45', '50'],
+    )
+    beds = st.selectbox(
+        "How many beds do you have?",
+        ("25", "50", "75", "100"),
+    )
+
+with col2:
+    name = st.text_input('What is the name of your hotel?', '''
+    ''')
+    email = st.text_input('What is the email of the administrator?', '''
+    ''')
+    bank_account =  st.text_input('Type your bank account:', '''
+    ''')
+    phone =  st.text_input('Type your phone number', '''
+    ''')
+    a = st.selectbox('How many beds for staff:', ['25', '50', '75', '100', '200'])
+
+
+b = st.selectbox('Monthly average expenditures by all the beds:', ['10.000€', '25.000€', '50.000€', '75.000€', '100.000€'])
+c = st.selectbox('Do you have budget for ads?:', ['Yes', 'No'])
+
+d = st.slider('How old are the locacies in years?', 0, 24)
+
+st.subheader('ATENTOS')
+col3, col4 = st.columns(2)
+
+with col3:
+    mood = st.selectbox(
+        "What mood do you prefer for your virtual staff?",
+        ("Elegant", "Friendly", "Formal", "Neutro", "Informal"),
+    )
+    channels = st.radio(
+        "What channels do you want to use for confirmations?",
+        options=['email', 'phone', 'twice'],
+    )
+    welcoming = st.selectbox(
+        "What kind of welcoming do you prefer to offer?",
+        ("Cold", "Neutro", "Warm"),
+    )
+
+with col4:
+    name = st.text_input('What is the day price of suits?', '''
+    ''')
+    email = st.text_input('What is the day price of the rooms?', '''
+    ''')
+    bank_account =  st.text_input('What is your rentability goal?', '''
+    ''')
+    phone =  st.text_input('What is your target?', '''
+    ''')
+    a = st.selectbox('Do you have support manifiesto?', ['Yes', 'No'])
+
+
+h = st.button('Crear Novus Hotel 🛏️ llamado ', name)
+
+if h:
+    st.write('¡Novus Hotel en construcción! Accede a los MANDOS y ATENTOS de tu <<', name, '>> con el código de confirmación enviado a <<', correo, '>>')
 
 
