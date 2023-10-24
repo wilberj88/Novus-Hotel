96f60b014a5fc7d5420cb21135615c57f7f88f7b
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-10-24 13:32:39 +0200

    Create Atentos.py

---
+++ pages/Atentos.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="ATENTOS Novus Hotel", page_icon="🛏️")
+
+st.title('ATENTOS 🛏️ Novus Hotel')
