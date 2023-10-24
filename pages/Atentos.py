c88860b5641ebbeb420e70d506235b7e998249ea
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-10-24 17:31:15 +0200

    Update Atentos.py

--- pages/Atentos.py
+++ pages/Atentos.py
@@ -16,51 +16,52 @@ st.set_page_config(layout="wide", page_title="ATENTOS Novus Hotel", page_icon="�
 st.title('ATENTOS 🛏️ Novus Hotel')
 
 current_time = time.ctime()
-st.write("In real time monitoring at: ", current_time)
+st.write("In real time operation at: ", current_time)
 
+if "messages" not in st.session_state:
+st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+with st.chat_message(message["user"]):
+    st.markdown(message["👋Bienvenido a Novus legal ⚖️: pregúntame lo que quieras sobre tu caso"])
+# React to user input
+if prompt := st.chat_input("Dímelo de una"):
+# Display user message in chat message container
+st.chat_message("user").markdown(prompt)
+# Add user message to chat history
+st.session_state.messages.append({"role": "user", "content": prompt})
+
+response = f"Echo: {prompt}"
+# Display assistant response in chat message container
+with st.chat_message("assistant"):
+    message_placeholder = st.empty()
+    full_response = ""
+    assistant_response = random.choice(
+        [
+            "Su consulta está ahora en las mejores manos, en breve enviamos reporte de consulta jurídica",
+            "Claro que sí, cuente con mi apoyo jurídico en todo su proceso judicial",
+            "Por supuesto que sí, estoy para defender todos tus derechos, cuentas conmigo",
+        ]
+    )
+    # Simulate stream of response with milliseconds delay
+    for chunk in assistant_response.split():
+        full_response += chunk + " "
+        time.sleep(0.1)
+        # Add a blinking cursor to simulate typing
+        message_placeholder.markdown(full_response + "▌")
+    message_placeholder.markdown(full_response)
+# Add assistant response to chat history
+st.session_state.messages.append({"role": "assistant", "content": full_response})
+        
+st.write('---')
 
 stage = st.selectbox("Choose the Stage of UX of 👤Clients👥 & 🤖Butlers🎩:",
         ("New Sales", "Confirmations", "Welcomings", "Support"),
     )
 
-if stage:
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
 
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["user"]):
-            st.markdown(message["👋Bienvenido a Novus legal ⚖️: pregúntame lo que quieras sobre tu caso"])
-    # React to user input
-    if prompt := st.chat_input("Dímelo de una"):
-        # Display user message in chat message container
-        st.chat_message("user").markdown(prompt)
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
-    
-        response = f"Echo: {prompt}"
-        # Display assistant response in chat message container
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            full_response = ""
-            assistant_response = random.choice(
-                [
-                    "Su consulta está ahora en las mejores manos, en breve enviamos reporte de consulta jurídica",
-                    "Claro que sí, cuente con mi apoyo jurídico en todo su proceso judicial",
-                    "Por supuesto que sí, estoy para defender todos tus derechos, cuentas conmigo",
-                ]
-            )
-            # Simulate stream of response with milliseconds delay
-            for chunk in assistant_response.split():
-                full_response += chunk + " "
-                time.sleep(0.1)
-                # Add a blinking cursor to simulate typing
-                message_placeholder.markdown(full_response + "▌")
-            message_placeholder.markdown(full_response)
-        # Add assistant response to chat history
-        st.session_state.messages.append({"role": "assistant", "content": full_response})
-        
-st.write('---')
+
 st.subheader(':blue[New Sales, Confirmation, Welcoming & Support Clients]👤')
 
 st.header('🤖🤵🏻Virtual Butlers🤵🏻‍♂️🤖')
