835ad586253d1f848c6364a31964ac6098a108a7
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-10-24 16:05:56 +0200

    Update Mandos.py

--- pages/Mandos.py
+++ pages/Mandos.py
@@ -175,6 +175,58 @@ col7, col8, col9 = st.columns(3)
 
 with col7:
     st.write('📢 Where to Advertise')
+    option = {
+        "legend": {},
+        "tooltip": {"trigger": "axis", "showContent": False},
+        "dataset": {
+            "source": [
+                ["product", "Monday", "Thursday", "Wednesday", "Tuesday", "Friday", "Saturday"],
+                ["TikTok", 56.5, 82.1, 88.7, 70.1, 53.4, 85.1],
+                ["Facebook & IG", 51.1, 51.4, 55.1, 53.3, 73.8, 68.7],
+                ["Google & Youtube", 40.1, 62.2, 69.5, 36.4, 45.2, 32.5],
+                ["Emailing", 25.2, 37.1, 41.2, 18, 33.9, 49.1],
+            ]
+        },
+        "xAxis": {"type": "category"},
+        "yAxis": {"gridIndex": 0},
+        "grid": {"top": "55%"},
+        "series": [
+            {
+                "type": "line",
+                "smooth": True,
+                "seriesLayoutBy": "row",
+                "emphasis": {"focus": "series"},
+            },
+            {
+                "type": "line",
+                "smooth": True,
+                "seriesLayoutBy": "row",
+                "emphasis": {"focus": "series"},
+            },
+            {
+                "type": "line",
+                "smooth": True,
+                "seriesLayoutBy": "row",
+                "emphasis": {"focus": "series"},
+            },
+            {
+                "type": "line",
+                "smooth": True,
+                "seriesLayoutBy": "row",
+                "emphasis": {"focus": "series"},
+            },
+            {
+                "type": "pie",
+                "id": "pie",
+                "radius": "30%",
+                "center": ["50%", "25%"],
+                "emphasis": {"focus": "data"},
+                "label": {"formatter": "{b}: {@2012} ({d}%)"},
+                "encode": {"itemName": "product", "value": "Lunes", "tooltip": "Lunes"},
+            },
+        ],
+    }
+    st_echarts(option, height="500px", key="echarts")
     
 with col8:
     st.write('🌹 Speech to Persuade')
